8cf61838d8c838046850c159e6fa9c11ad997dde
Noak Pettersson <noake12@gmail.com>
2023-02-06 17:30:04 +0100

    fetching from pokeapi working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Grid from './Grid';
 const Main = (props) => {
-  const NUMBER_OF_CARDS = 12;
+  const NUMBER_OF_CARDS = 12; // Possibility
 
   const getPokemons = async () => {
     const pokemonArray = [];
 
     for (let i = 1; i <= NUMBER_OF_CARDS; i++) {
       const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-      const response = await fetch(url, {mode: 'cors'});
+      const response = await fetch(url, { mode: 'cors' });
       const data = await response.json();
-      console.log(data);
+      const name = data.name;
+      const image = data.sprites.other.dream_world.front_default;
+      console.log(image);
+      pokemonArray.push({ name: name, image: image });
     }
   };
 
